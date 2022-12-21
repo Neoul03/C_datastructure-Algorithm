7d05bdb5461558a3aa5a5a33655d78fba1680d8f
Neoul03 <101254306+Neoul03@users.noreply.github.com>
2022-12-21 10:58:58 +0900

    Create cQueue.h

---
+++ CodeTest/CircularQueue/cQueue.h
@@ -0,0 +1,19 @@
+#pragma once
+#define cQ_SIZE 4
+
+typedef int element;
+
+typedef struct {
+	element queue[cQ_SIZE];
+	int front;
+	int rear;
+} QueueType;
+
+QueueType* createCQueue();
+int isEmpty(QueueType* cQ);
+int isFull(QueueType* cQ);
+void push(QueueType* cQ, element item);
+element pop(QueueType* cQ);
+element front(QueueType* cQ);
+element back(QueueType* cQ);
+void print(QueueType* cQ);
